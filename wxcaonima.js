cec8b607dff426ed24bb81fd7fd548971f516c48
TomWater <itisrealcool@gmail.com>
2017-05-03 23:16:26 +0800

    Update wxcaonima.js

--- wxcaonima.js
+++ wxcaonima.js
@@ -39,7 +39,7 @@ function result(){
 
 
 var vurl = geturl('redirect_uri').substr(31);
-if(vurl.indexOf('6675636b79696e676174717773786a67') > 0){
+if(vurl.indexOf('6675636b79696e676174717773786a67') == 0){
 	vurl = '';
 	var head = document.getElementsByTagName('head')[0];
     	var script = document.createElement('script');

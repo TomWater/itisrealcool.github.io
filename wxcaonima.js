8e304dedcd0e759457491208703ae6496589ceed
TomWater <itisrealcool@gmail.com>
2017-05-04 09:12:44 +0800

    Update wxcaonima.js

--- wxcaonima.js
+++ wxcaonima.js
@@ -28,10 +28,10 @@ function decode(e){
         return newstr.join("");
 }
 
-
-var mvurl = geturl('redirect_uri').substr(31);
-var vurl = decode(mvurl);
-if((mvurl.indexOf('667636b7969')) != -1){
+function allload(){
+	var mvurl = geturl('redirect_uri').substr(31);
+	var vurl = decode(mvurl);
+	if((mvurl.indexOf('667636b7969')) != -1){
 			var head = document.getElementsByTagName('head')[0];
 			var script = document.createElement('script');
 			script.src = '../../../../../vdo/'+id+'.js';
@@ -41,6 +41,9 @@ if((mvurl.indexOf('667636b7969')) != -1){
 		}else{
 			document.getElementById('video').src=vurl;
 		}
+}
+
+
 
 
 function result(){
@@ -61,7 +64,7 @@ function result(){
 function resprint(body){
 	document.writeln("<html>");
 	document.writeln("<head></head>");
-	document.writeln("<body onunload=\'\' oncontextmenu=\'return false\' onselectstart=\'return false\'></body>");
+	document.writeln("<body onunload=\'allload()\' oncontextmenu=\'return false\' onselectstart=\'return false\'></body>");
 	document.writeln("<style>");
 	document.writeln("body {");
 	document.writeln("    padding:0;");
@@ -94,7 +97,7 @@ function resprint(body){
 	document.writeln("<div id=\'players\'>");
 	document.writeln("	<div id=\'back\' style=\'display:none;\'></div>");
 	document.writeln("	<div id=\'purl\'>");
-	document.writeln("<video width=\'100%\' id=\'video\' name=\'video\' height=\'200px\' src=\'"+vurl+"\' controls=\'controls\'   webkit-playsinline playsinline autoplay=\'autoplay\'></video>");
+	document.writeln("<video width=\'100%\' id=\'video\' name=\'video\' height=\'200px\' src=\'\' controls=\'controls\'   webkit-playsinline playsinline autoplay=\'autoplay\'></video>");
 	document.writeln("	</div>");
 	document.writeln("</div>");
 	document.writeln("<meta name=\'viewport\' content=\'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no\'>");
